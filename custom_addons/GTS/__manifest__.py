353aad1656cc86cb99fb45a0c653add0208c3239
Priaranjan <priaranjan53@gmail.com>
2024-12-19 23:26:21 +0530

    Define GMS module and setup manifest

---
+++ custom_addons/GTS/__manifest__.py
@@ -0,0 +1,11 @@
+{
+    'name': "Generic Ticketing System",
+    'version': '1.0',
+    'depends': ['base'],
+    'author': "Rnajan",
+    'description': """
+    A Generic Ticketing System that can be used across various domains
+    """,
+    'license':'LGPL-3',
+    'application': True
+}
